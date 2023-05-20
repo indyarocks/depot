1ce5dd6fd733b8a1770725a215b35690ad3d6209
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-05-20 11:53:05 +0530

    finish caching

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select "table tbody tr", 3
   end
 
   test "should get new" do
